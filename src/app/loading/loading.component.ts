b85605ac614d22f7c8e67f9b693113ac921df3ce
Jeannuel Garcia <jjeanlink@gmail.com>
2021-07-22 12:49:35 -0500

    15. Loading Indication Service - Reactive Implementation

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -1,21 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs';
+import { LoadingService } from "./loading.service";
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'loading',
-  templateUrl: './loading.component.html',
-  styleUrls: ['./loading.component.css']
+  selector: "loading",
+  templateUrl: "./loading.component.html",
+  styleUrls: ["./loading.component.css"],
 })
 export class LoadingComponent implements OnInit {
+  constructor(public loadingService: LoadingService) {}
 
-
-  constructor() {
-
-  }
-
-  ngOnInit() {
-
-  }
-
-
+  ngOnInit() {}
 }
