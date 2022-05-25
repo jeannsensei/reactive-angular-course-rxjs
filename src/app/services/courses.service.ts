b3c819022a3663e7f13309cd59a487ecb77cba88
Jeannuel García <jegarcia@altimetrik.com>
2022-05-25 12:31:55 -0500

    Section 5: Master-Detail UI Pattern (with built-in state management)

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,25 +1,40 @@
-import { map, shareReplay } from 'rxjs/operators';
-import { Course } from './../model/course';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { map, shareReplay } from "rxjs/operators";
+import { Course } from "./../model/course";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Lesson } from "../model/lesson";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CoursesService {
-  constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) {}
 
   loadAllCourses(): Observable<Course[]> {
-    return this.http.get<Course[]>('/api/courses').pipe(
-      map(res => res['payload']),
+    return this.http.get<Course[]>("/api/courses").pipe(
+      map((res) => res["payload"]),
       shareReplay()
-    )
+    );
   }
 
   saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
-    return this.http.put(`/api/courses/${courseId}`, changes).pipe(
-      shareReplay()
-    );
+    return this.http
+      .put(`/api/courses/${courseId}`, changes)
+      .pipe(shareReplay());
+  }
+
+  searchLessons(search: string): Observable<Lesson[]> {
+    return this.http
+      .get<Lesson[]>("/api/lessons", {
+        params: {
+          filter: search,
+          pageSize: "100",
+        },
+      })
+      .pipe(
+        map((res) => res["payload"]),
+        shareReplay()
+      );
   }
 }
